7fd0e567aa879fe351eda326f0b8d8c3b19857b8
J. Pablo Pérez Trabucco <jpperez@ciudadanointeligente.org>
2012-06-22 12:13:26 -0400

    Adds condition to tables without relevant info

--- table_parser.py
+++ table_parser.py
@@ -1,45 +1,49 @@
+import sys
 import re
 import urllib
 import MLStripper
 	
 def py_parser(html):
 	#get html from url
-	#file = urllib.urlopen("http://www.senado.cl/appsenado/index.php?mo=sesionessala&ac=getDoctoSesion&iddocto=35414")
 	file = urllib.urlopen(html)
 	html = ""
 	for line in file:
 		html += line
 	#strip html tags
 	clean_html = MLStripper.strip_tags(html)
-
 	#get bill numbers
 	rx_bills = re.compile('\(.*Bolet(.*\d*\.{0,1}\d+-\d+)*.*\)')
 	bills = rx_bills.findall(clean_html)
-	bill_nums = []
-	rx_bill_num = re.compile('(\d{0,3})[^0-9]*(\d{0,3})[^0-9]*(\d{1,3})[^0-9]*(-)[^0-9]*(\d{2})')
-	for bill in bills:
-		for bill_num_array in rx_bill_num.findall(bill):
-			bill_num = "".join(bill_num_array)
-			bill_nums.append(bill_num)
+	#if the table is relevant
+	if len(bills) == 0:
+		bill_nums = []
+		rx_bill_num = re.compile('(\d{0,3})[^0-9]*(\d{0,3})[^0-9]*(\d{1,3})[^0-9]*(-)[^0-9]*(\d{2})')
+		for bill in bills:
+			for bill_num_array in rx_bill_num.findall(bill):
+				bill_num = "".join(bill_num_array)
+				bill_nums.append(bill_num)
 
-	#get date
-	rx_date = re.compile('(\d{1,2}) (?:de ){0,1}(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre) (?:de ){0,1}(\d{4})')
-	date = rx_date.findall(clean_html)
-	#date is array of arrays
-	date = date[0]
-	date = date_sp_2_en(date)
-	#get legislature
-	rx_legislature = re.compile('LEGISLATURA.+(\d{3})')
-	legislature = rx_legislature.findall(clean_html)
-	#get session
-	rx_session = re.compile('Sesi.+?(\d{1,2})')
-	session = rx_session.findall(clean_html)
+		#get date
+		rx_date = re.compile('(\d{1,2}) (?:de ){0,1}(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre) (?:de ){0,1}(\d{4})')
+		date = rx_date.findall(clean_html)
+		#date is array of arrays
+		date = date[0]
+		date = date_sp_2_en(date)
+		#get legislature
+		rx_legislature = re.compile('LEGISLATURA.+(\d{3})')
+		legislature = rx_legislature.findall(clean_html)
+		#get session
+		rx_session = re.compile('Sesi.+?(\d{1,2})')
+		session = rx_session.findall(clean_html)
 
-	#print to stdout
-	print "bill numbers: " + ",".join(bill_nums)
-	print "date: " + " ".join(date)
-	print "legislature: " + legislature[0]
-	print "session: " + session[0]
+		#print to stdout
+		print "bill numbers: " + ",".join(bill_nums)
+		print "date: " + " ".join(date)
+		print "legislature: " + legislature[0]
+		print "session: " + session[0]
+	
+	#if the table not contain bills information
+	print "is_not_a_relevant_table"
 
 #from d/m/y to m/d/y
 #month name from spanish to english
@@ -54,4 +58,5 @@ def date_sp_2_en(date):
 	return en_date
 
 if __name__ == '__main__':
-	py_parser()
+	print sys.argv[1]
+	py_parser(sys.argv[1])
