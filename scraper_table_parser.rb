098560b2a9eecad2748f12c046d5506f4b1e0c74
J. Pablo Pérez Trabucco <jpperez@ciudadanointeligente.org>
2012-06-25 19:18:11 -0400

    Fixes insertion to billit API

--- scraper_table_parser.rb
+++ scraper_table_parser.rb
@@ -5,7 +5,7 @@ require 'rest-client'
 require 'open-uri'
 
 class TableParserRobot
-	attr_accessor :html, :chamber, :lamb
+	attr_accessor :html, :chamber, :lamb, :session, :session_buffer
 
 	def initialize(html)
 		@html = html
@@ -14,6 +14,7 @@ class TableParserRobot
 		@lamb = lambda {|session, a| 
 			a.push(session)
 		}
+		@session_buffer = Array.new
 	end
 	def process
 		doc = Nokogiri::HTML(@html, nil, 'utf-8')
@@ -27,39 +28,64 @@ class TableParserRobot
 		table_url = tr.at_xpath('td[5]/a/@href').to_s.strip
 		begin
 			url = @base_url+table_url
-			file = open(url)
-			table_html = file.read
-			table_session = get_bills(table_html)
-			result = result.merge(table_session)
+			puts 'Procesando tabla (URL): '+url
+			#file = open(url)
+			#table_html = file.read # table_html
+			#puts 'table_html: '+table_html
+			table_session = get_bills(url)
+			if !table_session.nil?
+				puts 'El documento es una tabla de sesiones válida'
+				#puts '------------->'
+				#p table_session
+				#puts '<-------------'
+				result = table_session
+			end
 		rescue Exception=>e
-		end	
+		end
 		result
 	end
-	def get_bills(table_html = @html)
-		#llamar al metodo python, pasarle la url de la tabla
-		#y mapear los valores a un hash
+	def get_bills(url)
+		session = Hash.new
+		#exec python script
+		table_values = %x[python table_parser.py '#{url}'].gsub(/\n/,' ')
+		#puts 'table_values: '+table_values
+
+		if !(table_values.match(/skipped/))
+			session['session'] = table_values.scan(/session: (\d*)/).flatten[0]
+			session['legislature'] = table_values.scan(/legislature: (\d*)/).flatten[0]
+			session['date'] = table_values.scan(/date: (\w*) (\d{2}) (\d{4})/).join(' ')
+			session['bills'] = table_values.scan(/bill numbers: (\S*)/).flatten[0].split(/,/)
+			session['chamber'] = @chamber
+
+			session
+		end
 	end
 end
 
 if !(defined? Test::Unit::TestCase)
 	url = 'http://www.senado.cl/appsenado/index.php?mo=sesionessala&ac=doctosSesion&tipo=27'
-	puts '1/3 Descargando la tabla de sesiones de www.senado.cl...'
+	puts '1/3 Descargando la tabla de sesiones de www.senado.cl'
 	file = open(url)
 	puts '2/3 Procesando el documento...'
 	html = file.read
 	robot = TableParserRobot.new(html)
 	robot.lamb = lambda {|session, a|
-		url = 'http://api.ciudadanointeligente.cl/billit/cl/bills'
-		#usar un metodo para pasar a ingles la fecha si es que falla en espanol
+		url = 'http://api.ciudadanointeligente.cl/billit/cl/tables'
 		a.push(session)
-		data = {
-			:date => session["date"],
-			:chamber => session["chamber"],
-			:legislature => session["legislature"],
-			:session => session["session"]
-		}
-		#RestClient.put url, data, {:content_type => :json}
+		if !session.nil?
+			data = {
+				:id => session['legislature'].concat('-'+session['session']),
+				:date => session['date'],
+				:chamber => session['chamber'],
+				:legislature => session['legislature'],
+				:session => session['session']
+			}
+			#puts '<------------'
+			#p data
+			#puts '------------>'
+			RestClient.put url, data, {:content_type => :json}
+		end
 	}
-	result = robot.process
-	puts '3/3 Finalizado'
+	resultado = robot.process
+	puts '3/3 Finalizado.'
 end
