f92f470f8db695f00998e4f326b6e8a1a1d60a66
J. Pablo Pérez Trabucco <jpperez@ciudadanointeligente.org>
2012-06-25 19:16:40 -0400

    Modifies to handle tables without information

--- table_parser.py
+++ table_parser.py
@@ -15,35 +15,42 @@ def py_parser(html):
 	rx_bills = re.compile('\(.*Bolet(.*\d*\.{0,1}\d+-\d+)*.*\)')
 	bills = rx_bills.findall(clean_html)
 	#if the table is relevant
-	if len(bills) == 0:
-		bill_nums = []
-		rx_bill_num = re.compile('(\d{0,3})[^0-9]*(\d{0,3})[^0-9]*(\d{1,3})[^0-9]*(-)[^0-9]*(\d{2})')
-		for bill in bills:
-			for bill_num_array in rx_bill_num.findall(bill):
-				bill_num = "".join(bill_num_array)
-				bill_nums.append(bill_num)
+	if len(bills) != 0:
+		process_table(clean_html)
+	else:
+		#if the table not contain bills information
+		print "skipped"
 
-		#get date
-		rx_date = re.compile('(\d{1,2}) (?:de ){0,1}(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre) (?:de ){0,1}(\d{4})')
-		date = rx_date.findall(clean_html)
-		#date is array of arrays
-		date = date[0]
-		date = date_sp_2_en(date)
-		#get legislature
-		rx_legislature = re.compile('LEGISLATURA.+(\d{3})')
-		legislature = rx_legislature.findall(clean_html)
-		#get session
-		rx_session = re.compile('Sesi.+?(\d{1,2})')
-		session = rx_session.findall(clean_html)
+#only use in case of relevant tables
+def process_table(clean_html):
+	rx_bills = re.compile('\(.*Bolet(.*\d*\.{0,1}\d+-\d+)*.*\)')
+	bills = rx_bills.findall(clean_html)
 
-		#print to stdout
-		print "bill numbers: " + ",".join(bill_nums)
-		print "date: " + " ".join(date)
-		print "legislature: " + legislature[0]
-		print "session: " + session[0]
-	
-	#if the table not contain bills information
-	print "is_not_a_relevant_table"
+	bill_nums = []
+	rx_bill_num = re.compile('(\d{0,3})[^0-9]*(\d{0,3})[^0-9]*(\d{1,3})[^0-9]*(-)[^0-9]*(\d{2})')
+	for bill in bills:
+		for bill_num_array in rx_bill_num.findall(bill):
+			bill_num = "".join(bill_num_array)
+			bill_nums.append(bill_num)
+
+	#get date
+	rx_date = re.compile('(\d{1,2}) (?:de ){0,1}(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre) (?:de ){0,1}(\d{4})')
+	date = rx_date.findall(clean_html)
+	#date is array of arrays
+	date = date[0]
+	date = date_sp_2_en(date)
+	#get legislature
+	rx_legislature = re.compile('LEGISLATURA.+(\d{3})')
+	legislature = rx_legislature.findall(clean_html)
+	#get session
+	rx_session = re.compile('Sesi.+?(\d{1,2})')
+	session = rx_session.findall(clean_html)
+
+	#print to stdout
+	print "bill numbers: " + ",".join(bill_nums)
+	print "date: " + " ".join(date)
+	print "legislature: " + legislature[0]
+	print "session: " + session[0]
 
 #from d/m/y to m/d/y
 #month name from spanish to english
@@ -58,5 +65,7 @@ def date_sp_2_en(date):
 	return en_date
 
 if __name__ == '__main__':
-	print sys.argv[1]
+	#print '--------------------------->'
+	#print sys.argv[1]
+	#print '<---------------------------'
 	py_parser(sys.argv[1])
